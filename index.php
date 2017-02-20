167a95912712d0b7337f4ca2fe6f6d64bf4dead2
Merel Kokkeler <kokkelermerel@gmail.com>
2017-02-20 10:34:03 +0100

    menu

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nutellie
+ * Date: 2/8/2017
+ * Time: 1:57 PM
+ */
+?>
+
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="styles/style.css" />
+    </head>
+
+    <body>
+        <?php
+            include("menu.php");
+        ?>
+    </body>
+</html>
