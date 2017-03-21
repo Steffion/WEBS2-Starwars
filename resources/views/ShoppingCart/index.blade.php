9d791a08975e9e170ca0a38c29c8213a74c13d14
Merel Kokkeler <kokkelermerel@gmail.com>
2017-03-21 13:43:37 +0100

    Added shoppingcart

---
+++ resources/views/ShoppingCart/index.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kokke
+ * Date: 20-3-2017
+ * Time: 15:05
+ */
+?>
+
+<p>
+
+
+</p>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -27,7 +27,7 @@
                 <a class="nav-link" href="/sale">Sale</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Shopping Cart</a>
+                <a class="nav-link" href="/cart">Shopping Cart</a>
             </li>
         </ul>
     </div>
