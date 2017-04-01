9fc626f66ea0c321805e0500daf0384bd0dd70dd
Merel Kokkeler <kokkelermerel@gmail.com>
2017-04-01 19:45:23 +0200

    Account settings interface

--- resources/views/dashboard/account/index.blade.php
+++ resources/views/dashboard/account/index.blade.php
@@ -1,1 +1,47 @@
-THIS IS ACCOUNT SETTINGS PAGE
+<!-- Text input-->
+<div class="form-group">
+    <label class="col-md-4 control-label" for="textinput">Change Email ID</label>
+    <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="your current email " class="form-control input-md">
+        <span class="help-block">your new email ID</span>
+    </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+    <label class="col-md-4 control-label" for="passwordinput">new password</label>
+    <div class="col-md-4">
+        <input id="passwordinput" name="passwordinput" type="password" placeholder="new password" class="form-control input-md">
+
+    </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+    <label class="col-md-4 control-label" for="passwordinput">repeat password</label>
+    <div class="col-md-4">
+        <input id="passwordinput" name="passwordinput" type="password" placeholder="repeat password" class="form-control input-md">
+
+    </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+    <label class="col-md-4 control-label" for="singlebutton">view account status</label>
+    <div class="col-md-4">
+        <button id="singlebutton" name="singlebutton" class="btn btn-success">view</button>
+    </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+    <label class="col-md-4 control-label" for="singlebutton">Remove my account</label>
+    <div class="col-md-4">
+        <button id="singlebutton" name="singlebutton" class="btn btn-danger">remove</button>
+    </div>
+</div>
+
+</fieldset>
+</form>
+
+</div>
