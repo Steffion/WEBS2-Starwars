87013dd1c0eefe2ce6d7f47a92e11d5b8747f00d
Merel Kokkeler <kokkelermerel@gmail.com>
2017-04-02 14:13:48 +0200

    Checkout page

---
+++ resources/views/ShoppingCart/checkout.blade.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nutellie
+ * Date: 4/2/2017
+ * Time: 2:08 PM
+ */
+?>
+
+
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="jumbotron">
+        <div class="container">
+            <h2>Your order has been recieved</h2>
+            <p> Order Number:   </p>
+
+        </div>
+    </div>
+
+
+    <img src="img/thanks.png" class="mx-auto">
+
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
         @foreach($products as $product)
             <div class="col-lg-3">
                 <h2> {{ $product->name }} </h2>
-                <img id="RO" class="grow" src="data:image/*;base64,{{ $product->thumbnail }}"/>
+                <img id="RO" src="data:image/*;base64,{{ $product->thumbnail }}"/>
 
                 {{ $product->description }}
 
